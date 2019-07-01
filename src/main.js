d8bf03d105562e2e6e9ad31a38e761b3e8a09146
spemer <ghsspower@gmail.com>
2019-07-01 22:26:47 +0900

    FIX lodash -> VueLodash

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import store from '@/store'
 
 import '@/registerServiceWorker'
 
-import {
-  find,
-  findIndex
-} from 'lodash'
+import VueLodash from 'vue-lodash'
+Vue.use(VueLodash)
 
 Vue.config.productionTip = false
 
